ea93e1d0e227772ef3946fc7ea9c4f98c58da34c
SEMIP <SEMIP@DESKTOP-1>
2025-09-09 00:20:49 +0300

    update

---
+++ Areas/Admin/Controllers/CinemaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MAXCINA.Areas.Admin.Controllers
+{
+    [Area(SD.Area_Admin)]
+    public class CinemaController : Controller
+    {
+        private ApplicationDbContext _context = new();
+        public IActionResult Index()
+        {
+            var cinema = _context.Cinemas.OrderBy(m => m.Id);
+            return View(cinema.ToList());
+        }
+        //----------------------------------------------------------------
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
